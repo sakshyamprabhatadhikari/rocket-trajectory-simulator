420c57fd4441e17f338be5a76cafe863668b8af4
sakshyamprabhatadhikari <sakshyamlinkedin2082@gmail.com>
2026-01-16 14:05:03 +0545

    Updated Main.java and configured application entry point and launch sequence

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package main;
 
-/**
- *
- * @author saksh
- */
-public class Main {
+import javax.swing.UIManager;
+import view.WelcomeFrame;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // continue with default if Nimbus fails
+        }
+
+        java.awt.EventQueue.invokeLater(() -> {
+            WelcomeFrame wf = new WelcomeFrame();
+            wf.setLocationRelativeTo(null); // optional, not against guidelines
+            wf.setVisible(true);
+        });
     }
-    
 }
